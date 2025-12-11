9723224690e9651acd249a93f0298628797b7a4a
veer-verify <veer.verify@gmail.com>
2025-12-11 10:34:07 +0530

    test

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -19,6 +19,7 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | UrlTree {
+    
     if (this.auth.isLoggedIn()) {
       return true;
     }
